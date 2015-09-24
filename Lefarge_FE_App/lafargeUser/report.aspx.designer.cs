feb75048ca2ee0b820cd5b3c22634c179bb7a5bb
kDuffy93 <kyleduffy83@gmail.com>
2015-09-23 22:45:27 -0400

    test

--- Lefarge_FE_App/lafargeUser/report.aspx.designer.cs
+++ Lefarge_FE_App/lafargeUser/report.aspx.designer.cs
@@ -67,15 +67,6 @@ namespace Lefarge_FE_App.admin {
         protected global::System.Web.UI.WebControls.Button btnDifPlant;
         
         /// <summary>
-        /// btndifequip control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btndifequip;
-        
-        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
