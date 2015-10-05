3ad88722fbce918acb5710927c71830f7b4cec9e
kDuffy93 <kyleduffy83@gmail.com>
2015-10-05 18:58:17 -0400

    timeout

--- Lefarge_FE_App/lafargeUser/report.aspx.designer.cs
+++ Lefarge_FE_App/lafargeUser/report.aspx.designer.cs
@@ -85,6 +85,15 @@ namespace Lefarge_FE_App.admin {
         protected global::System.Web.UI.WebControls.Panel Panel1;
         
         /// <summary>
+        /// dlPictures control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList dlPictures;
+        
+        /// <summary>
         /// OKButton control.
         /// </summary>
         /// <remarks>
